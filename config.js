8ad0916900d25b45f5b31a3b23d071c559d2d4bb
epigani <emanuele.pigani@me.com>
2026-07-08 22:02:47 +0200

    Update download links for beta 12

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 window.PaperBarConfig = {
-  koFiURL: "https://ko-fi.com/paperbarapp"
+  koFiURL: "https://ko-fi.com/paperbarapp",
+  reviewURL: "mailto:contact@paperbar.app?subject=PaperBar%20review",
+  websiteURL: "https://epigani.github.io/paperbar-site/"
 };
