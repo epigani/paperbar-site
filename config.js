ca553b779221cfa5fe8f8ce15338bcb2fe66d682
epigani <emanuele.pigani@me.com>
2026-07-09 16:09:05 +0200

    Add beta feedback and support actions

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.PaperBarConfig = {
   koFiURL: "https://ko-fi.com/paperbarapp",
-  reviewURL: "mailto:contact@paperbar.app?subject=PaperBar%20review",
+  reviewURL: "mailto:contact@paperbar.app?subject=PaperBar%20beta%20feedback",
   websiteURL: "https://epigani.github.io/paperbar-site/"
 };
